4f37a93379bd931a9ac0281f9aad682652394f9a
Ayoub <ayoubkhodar1998@gmail.com>
2021-01-23 10:51:33 +0100

    Exam React Native

---
+++ sourceFolder1/sourceFolder2/data.js
@@ -0,0 +1,13 @@
+ const data=[{key:'1',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'2',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'3',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'4',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'5',nom:'ayoub', quantite:'25', prixunit:'120'}];
+
+export const data1=[{key:'1',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'2',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'3',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'4',nom:'ayoub', quantite:'25', prixunit:'120'},
+{key:'5',nom:'ayoub', quantite:'25', prixunit:'120'}];
+
+export default data;
